e80b09e1a73a91206fa162d6172fe7452420aafb
Musyimi <musyimi13@gmail.com>
2023-03-13 14:34:38 +0300

    corrected the url mistake in api.js

--- src/api.js
+++ src/api.js
@@ -1,17 +1,19 @@
 import axios from 'axios';
 
-const searchImages = async (term) => {
-  const response = await axios.get('https://api/unsplash.com/search/photos', {
+
+const searchImages = async () => {
+  const response = await axios.get('https://api.unsplash.com/search/photos', {
     headers: {
        Authorization: 'Client-ID J8hPf6fn5gXuFj32gLy7H28Fz761ZUGUG8_CzO6AQMw',
     },
     params: {
-       query: term,
+       query: 'cars',
     },
    });
 
+   console.log(response);
 
-   return response.data.results;
+   return response;
 };
 
 export default searchImages;
